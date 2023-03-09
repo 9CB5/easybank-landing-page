cbc6ef02e66113582627efa1313949a1d9d27af0
Christian Barcelona <christianbarcelona95@gmail.com>
2023-03-09 13:22:40 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Easybank landing page
+
+Easybank landing page made with SASS using BEM and 7-1 pattern.
+
+## Live link
+
+[Insert live link here]
+
+## Installation
+
+Run the command below to install the dependencies.
+
+```bash
+npm install
+```
+
+## Usage
+
+To run the project locally and compile changes, run the following command:
+
+```bash
+npm run compile:sass
+```
---
+++ script.js
@@ -0,0 +1,76 @@
+// Functions
+// ================================================================================================
+
+// Toggle background image in the header
+// ------------------------------------------------------------------------------------------------
+function bgToggle(source = "./images/bg-intro-desktop.svg") {
+    const imageElement = document.getElementById("header__image-box--bg");
+
+    imageElement.setAttribute("src", source);
+}
+
+// Toggle nav icon
+// ------------------------------------------------------------------------------------------------
+function toggleNavIcon() {
+    const icon = document.querySelector(".navigation__burger");
+    const source = icon.getAttribute("src");
+
+    if (source === "./images/icon-hamburger.svg") {
+        icon.src = "./images/icon-close.svg";
+    } else {
+        icon.src = "./images/icon-hamburger.svg";
+    }
+}
+
+function navToggle() {
+    const burger = document.querySelector('.navigation__burger');
+    const navList = document.querySelector(".navigation__list");
+
+    burger.addEventListener('click', () => {
+        navList.classList.toggle("show");
+
+        toggleNavIcon();
+    });
+}
+
+// Event listeners
+// ================================================================================================
+window.onresize = () => {
+    const btnNav = document.querySelector(".btn-nav");
+
+    if (window.innerWidth < 768) {
+        bgToggle("./images/bg-intro-mobile.svg");
+        btnNav.classList.add("hide");
+    } else {
+        bgToggle();
+        btnNav.classList.remove("hide");
+    }
+}
+
+window.onload = () => {
+    const btnNav = document.querySelector(".btn-nav")
+
+    if (window.innerWidth < 768) {
+        bgToggle("./images/bg-intro-mobile.svg");
+        btnNav.classList.add("hide");
+    } else {
+        bgToggle();
+        btnNav.classList.remove("hide");
+    }
+}
+
+// Main method
+// ================================================================================================
+function main() {
+    // Adds an event listener to the burger
+    // --------------------------------------------------------------------------------------------
+    navToggle();
+
+    // Toggle the background image in the header
+    // --------------------------------------------------------------------------------------------
+    bgToggle();
+}
+
+// Main method call
+// ================================================================================================
+main();
